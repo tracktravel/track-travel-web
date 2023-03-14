adbb6ee76780739774c78ccb147880db925571db
Florencia Fernandez <96849628+FlorenciaF1@users.noreply.github.com>
2023-03-14 15:52:56 -0300

    Upload

--- tt-web/src/components/home-description/HomeDescription.jsx
+++ tt-web/src/components/home-description/HomeDescription.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {BsShieldFillCheck, BsCheckCircleFill, BsController} from "react-icons/bs";
+import { BsShieldFillCheck, BsCheckCircleFill, BsController } from "react-icons/bs";
 import { GiGears } from "react-icons/gi";
 import { FaHandshake } from "react-icons/fa";
 import styles from "./HomeDescription.module.css";
@@ -10,35 +10,41 @@ const HomeDescription = () => {
       <div className={styles.titleContainer}>
         <h2 className={styles.title}>Track Travel</h2>
         <p className={styles.titleDescription}>
-        Track Travel es una aplicación web que permite a los usuarios rastrear su historial de viajes. Los usuarios pueden crear una cuenta y iniciar sesión en su historial de viajes. También pueden ver su historial de viajes y editarlo.
+          Track Travel es una aplicación web que permite a los usuarios rastrear su historial de viajes. Los usuarios pueden crear una cuenta y iniciar sesión en su historial de viajes. También pueden ver su historial de viajes y editarlo.
         </p>
       </div>
       <div className={styles.divs}>
         <div>
-          <h3>
-            <BsCheckCircleFill className={styles.items}/>
-            Simplicidad
-          </h3>
+          <div className={styles.cards}>
+            <BsCheckCircleFill className={styles.items} />
+            <h3>
+              Simplicidad
+            </h3>
+          </div>
           <p>
-          Track Travel permite a los usuarios rastrear su historial de viajes de una manera sencilla y fácil de usar. 
+            Track Travel permite a los usuarios rastrear su historial de viajes de una manera sencilla y fácil de usar.
           </p>
         </div>
         <div>
-          <h3>
-            <BsShieldFillCheck className={styles.items}/>
-            Seguridad
-          </h3>
+          <div className={styles.cards}>
+            <BsShieldFillCheck className={styles.items} />
+            <h3>
+              Seguridad   
+            </h3>
+          </div>
           <p>
-          Track Travel es una aplicación web segura que permite a los usuarios rastrear su viaje sin preocuparse por la seguridad de sus datos.
+            Track Travel es una aplicación web segura que permite a los usuarios rastrear su viaje sin preocuparse por la seguridad de sus datos.
           </p>
         </div>
         <div>
-          <h3>
-            <GiGears className={styles.items}/>
-            Automatización
-          </h3>
+          <div className={styles.cards}>
+            <GiGears className={styles.items} />
+            <h3>
+              Automatización
+            </h3>
+          </div>
           <p>
-          Track Travel permite a los usuarios rastrear su viaje de manera automática, dejando la tarea de rastrear su viaje a la aplicación. De esta manera, los usuarios pueden concentrarse en disfrutar de su viaje.
+            Track Travel permite a los usuarios rastrear su viaje de manera automática, dejando la tarea de rastrear su viaje a la aplicación. De esta manera, los usuarios pueden concentrarse en disfrutar de su viaje.
           </p>
         </div>
         {/* <div>
@@ -51,12 +57,14 @@ const HomeDescription = () => {
           </p>
         </div> */}
         <div>
-          <h3>
-            <FaHandshake className={styles.items}/>
-            Confianza
-          </h3>
+          <div className={styles.cards}>
+            <FaHandshake className={styles.items} />
+            <h3>
+              Confianza
+            </h3>
+          </div>
           <p>
-          Track Travel es una aplicación web que permite a los usuarios rastrear su viaje con confianza, ya que los datos de los usuarios están protegidos y no se comparten con terceros.
+            Track Travel es una aplicación web que permite a los usuarios rastrear su viaje con confianza, ya que los datos de los usuarios están protegidos y no se comparten con terceros.
           </p>
         </div>
       </div>
