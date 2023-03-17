0ca9192a93b1adb660339d1d5705dd350dd0a1e9
Emiliano Villa <villaemiliano274@gmail.com>
2023-03-17 12:46:48 -0300

    feat/slider-hero

--- tt-web/src/components/hero/Hero.jsx
+++ tt-web/src/components/hero/Hero.jsx
@@ -8,7 +8,7 @@ import handleScroll from '@/helpers/HandleScroll';
 
 const Hero = () => {
   return (
-    <div className={`${styles.container} `}>
+    <div className={styles.container}>
       <div className={styles.containerText}>
         <h1 className={styles.title}>Tours más inteligentes para tu agencia</h1>
         <Link href='#home-description' onClick={handleScroll}><button className={styles.button}>Saber más</button></Link>
