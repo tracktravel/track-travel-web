4bb112e5fbd852cc34a8e90c5c062b80fd77d3e4
02fede02 <fede.birman93@gmail.com>
2023-03-29 12:42:17 -0300

    feat:created allies and explore pages

---
+++ tt-web/src/pages/explore/index.jsx
@@ -0,0 +1,10 @@
+import UnderConstruction from '@/components/underConstruction/UnderConstruction'
+import React from 'react'
+
+const index = () => {
+  return (
+    <UnderConstruction />
+  )
+}
+
+export default index
