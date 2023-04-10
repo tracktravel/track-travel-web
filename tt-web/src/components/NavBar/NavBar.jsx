23235979903fa6b5ee5df85c9197430f6bf65e6c
unknown <juanjc2002@hotmail.com>
2023-04-10 20:41:30 -0300

    feat: darkMode/lightMode

--- tt-web/src/components/NavBar/NavBar.jsx
+++ tt-web/src/components/NavBar/NavBar.jsx
@@ -4,66 +4,68 @@ import Logo from "@/components/logo/Logo";
 import { GiHamburgerMenu } from "react-icons/gi";
 import styles from "./NavBar.module.css";
 import { useRouter } from "next/router";
+import { handleModeSwitch } from "../../helpers/modeSwitch"
 
 export default function NavBar() {
-  const router = useRouter()
-  const currentRoute = router.pathname
+	const router = useRouter();
+	const currentRoute = router.pathname;
+  
+  const [mode, setMode] = useState(true)
+	const [menu, setMenu] = useState(false);
 
-  const [menu, setMenu] = useState(false);
+	const toggleMenu = () => {
+		setMenu(!menu);
+	};
 
-  const toggleMenu = () => {
-    setMenu(!menu);
-  };
+	return (
+		<div className={styles.background}>
+			<header className={styles.cabecera}>
+				<div>
+					<Logo />
+				</div>
 
-  return (
-    <div className={styles.background}>
-      <header className={styles.cabecera}>
-        <div>
-          <Logo />
-        </div>
+				<button onClick={toggleMenu} className={styles.cabeceraButton}>
+					<GiHamburgerMenu className={styles.cabeceraSvg} />
+				</button>
 
-        <button onClick={toggleMenu} className={styles.cabeceraButton}>
-          <GiHamburgerMenu className={styles.cabeceraSvg} />
-        </button>
-
-        <nav className={`${styles.cabeceraNav} ${menu ? styles.isActive : ""}`}>
-          <ul className={styles.cabeceraUl}>
-            <li
-              className={`${styles.cabeceraLi} ${currentRoute === "/" ? styles.cabeceraLiActive : ""
-                }`}
-            >
-              <Link
-                className={styles.cabeceraA}
-                href="/"
-              >
-                Home
-              </Link>
-            </li>
-            <li
-              className={`${styles.cabeceraLi} ${currentRoute === "/about" ? styles.cabeceraLiActive : ""
-                }`}
-            >
-              <Link
-                className={styles.cabeceraA}
-                href="/about"
-              >
-                About
-              </Link>
-            </li>
-            <li
-              className={`${styles.cabeceraLi} ${currentRoute === "/contact" ? styles.cabeceraLiActive : ""
-                }`}
-            >
-              <Link
-                className={styles.cabeceraA}
-                href="/contact"
-              >
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </div>
-  );
+				<nav className={`${styles.cabeceraNav} ${menu ? styles.isActive : ""}`}>
+					<ul className={styles.cabeceraUl}>
+						<li
+							className={`${styles.cabeceraLi} ${
+								currentRoute === "/" ? styles.cabeceraLiActive : ""
+							}`}
+						>
+							<Link className={styles.cabeceraA} href="/">
+								Home
+							</Link>
+						</li>
+						<li
+							className={`${styles.cabeceraLi} ${
+								currentRoute === "/about" ? styles.cabeceraLiActive : ""
+							}`}
+						>
+							<Link className={styles.cabeceraA} href="/about">
+								About
+							</Link>
+						</li>
+						<li
+							className={`${styles.cabeceraLi} ${
+								currentRoute === "/contact" ? styles.cabeceraLiActive : ""
+							}`}
+						>
+							<Link className={styles.cabeceraA} href="/contact">
+								Contact
+							</Link>
+						</li>
+						<li>
+							<label className={styles.switch} onChange={(e) => handleModeSwitch(e)}>
+								<input type="checkbox"></input>
+								<span className={styles.slider}></span>
+							</label>
+						</li>
+					</ul>
+				</nav>
+			</header>
+		</div>
+	);
 }
