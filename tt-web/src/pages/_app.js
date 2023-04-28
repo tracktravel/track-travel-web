b4324fb89f795658fee49d892e04f0b8550db005
Emiliano Villa <villaemiliano274@gmail.com>
2023-04-28 11:37:58 -0300

    feat/translation

--- tt-web/src/pages/_app.js
+++ tt-web/src/pages/_app.js
@@ -1,8 +1,36 @@
 import "@/styles/globals.css";
 import Layout from "@/layout/Layout";
 import Loader from "@/components/loader/Loader";
-import { applyInitialDataTheme } from "../helpers/initialTheme"
+import { applyInitialDataTheme } from "../helpers/initialTheme";
 import { useEffect, useState } from "react";
+// importaciones para i18n
+import { I18nextProvider } from "react-i18next";
+import i18next from "i18next";
+
+// importacion de los archivos json de los idiomas
+import global_es from "../translate/es.json";
+import global_en from "../translate/en.json";
+import global_pt from "../translate/pt.json";
+
+i18next.init({
+  interpolation: { escapeValue: false },
+
+  lng: 'es',  
+  resources: {
+    es: {
+     global: global_es    
+       },
+    
+    en: {
+      global: global_en
+    },  
+    
+    pt: {
+      global: global_pt
+   }
+    } 
+  }) 
+
 
 export default function App({ Component, pageProps }) {
   let [load, setLoad] = useState(null)
@@ -21,8 +49,10 @@ export default function App({ Component, pageProps }) {
     )
   }else 
   return (
+    <I18nextProvider i18n={i18next}> 
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </I18nextProvider>
   );
 }
