17d9e352e65902cf8f45c9bbe9531bdfe4aa88c2
Juan Cruz Laumann <109559830+JuanCruzL@users.noreply.github.com>
2023-05-02 14:18:26 -0300

    Update initialTheme.js

--- tt-web/src/helpers/initialTheme.js
+++ tt-web/src/helpers/initialTheme.js
@@ -1,8 +1,6 @@
 const applyInitialDataTheme = () => {
   if (!localStorage.getItem("theme")) {
     localStorage.setItem("theme", "light")
-    document.getElementById("switch").className = "light"
-    document.documentElement.setAttribute("data-theme", "light")
   } else if(localStorage.getItem("theme")) {
     let mode = localStorage.getItem("theme")
     document.getElementById("switch").className = mode
