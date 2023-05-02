575df53b34d27ddbefffa52f958470f938eeb642
unknown <juanjc2002@hotmail.com>
2023-05-02 13:49:14 -0300

    fix: fixed initial theme problem

--- tt-web/src/helpers/initialTheme.js
+++ tt-web/src/helpers/initialTheme.js
@@ -1,13 +1,13 @@
 const applyInitialDataTheme = () => {
   if (!localStorage.getItem("theme")) {
     localStorage.setItem("theme", "light")
+    document.getElementById("switch").className = "light"
+    document.documentElement.setAttribute("data-theme", "light")
   } else if(localStorage.getItem("theme")) {
     let mode = localStorage.getItem("theme")
     document.getElementById("switch").className = mode
     document.documentElement.setAttribute("data-theme", mode)
   }
 }
-
-
 
 module.exports = { applyInitialDataTheme }
