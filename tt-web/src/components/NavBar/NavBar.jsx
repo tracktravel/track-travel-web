eb633f59d309e614239ed272e688d871916c301d
02fede02 <fede.birman93@gmail.com>
2023-05-04 14:47:06 -0300

    hotfix/ translate in pt

--- tt-web/src/components/NavBar/NavBar.jsx
+++ tt-web/src/components/NavBar/NavBar.jsx
@@ -4,82 +4,97 @@ import Logo from "@/components/logo/Logo";
 import { GiHamburgerMenu } from "react-icons/gi";
 import styles from "./NavBar.module.css";
 import { useRouter } from "next/router";
-import {useTranslation} from "react-i18next" 
+import { useTranslation } from "react-i18next";
+import LanguageDropdown from "../languageDropdown/LanguageDropdown";
 
 export default function NavBar() {
-	const router = useRouter();
-	const currentRoute = router.pathname;
+  const router = useRouter();
+  const currentRoute = router.pathname;
 
-  const [t, i18next] = useTranslation("global")
-	const [theme, setTheme] = useState(null)
-	const [menu, setMenu] = useState(false);
+  const [t, i18next] = useTranslation("global");
+  const [theme, setTheme] = useState(null);
+  const [menu, setMenu] = useState(false);
 
-	useEffect(() => {
-		setTheme(localStorage.getItem("theme") === "light" ? false : true)
-	},[])
+  useEffect(() => {
+    setTheme(localStorage.getItem("theme") === "light" ? false : true);
+  }, []);
 
-	const toggleMenu = () => {
-		setMenu(!menu);
-	};
+  const toggleMenu = () => {
+    setMenu(!menu);
+  };
 
-	const handleModeSwitch = (e) => {
-		let mode = e.target.checked ? "dark" : "light"
-		localStorage.setItem("theme", mode)
-		let body = document.getElementById("switch")
-		body.className = localStorage.getItem("theme")
-		document.documentElement.setAttribute("data-theme", mode)
-		setTheme(!theme)
-	}
+  const handleModeSwitch = (e) => {
+    let mode = e.target.checked ? "dark" : "light";
+    localStorage.setItem("theme", mode);
+    let body = document.getElementById("switch");
+    body.className = localStorage.getItem("theme");
+    document.documentElement.setAttribute("data-theme", mode);
+    setTheme(!theme);
+  };
 
-	return (
-		<div className={styles.background}>
-			<header className={styles.cabecera}>
-				<div>
-					<Logo />
-				</div>
+  return (
+    <div className={styles.background}>
+      <header className={styles.cabecera}>
+        <div>
+          <Logo />
+        </div>
 
-				<button onClick={toggleMenu} className={styles.cabeceraButton}>
-					<GiHamburgerMenu className={styles.cabeceraSvg} />
-				</button>
+        <button onClick={toggleMenu} className={styles.cabeceraButton}>
+          <GiHamburgerMenu className={styles.cabeceraSvg} />
+        </button>
 
-				<nav className={`${styles.cabeceraNav} ${menu ? styles.isActive : ""}`}>
-					<ul className={styles.cabeceraUl}>
-						<li
-							className={`${styles.cabeceraLi} ${
-								currentRoute === "/" ? styles.cabeceraLiActive : ""
-							}`}
-						>
-							<Link className={styles.cabeceraA} href="/">
-								{t("navbar.home")}
-							</Link>
-						</li>
-						<li
-							className={`${styles.cabeceraLi} ${
-								currentRoute === "/about" ? styles.cabeceraLiActive : ""
-							}`}
-						>
-							<Link className={styles.cabeceraA} href="/about">
-								{t("navbar.about")}
-							</Link>
-						</li>
-						<li
-							className={`${styles.cabeceraLi} ${
-								currentRoute === "/contact" ? styles.cabeceraLiActive : ""
-							}`}
-						>
-							<Link className={styles.cabeceraA} href="/contact">
-								{t("navbar.contact")}
-							</Link>
-						</li>
-						<li>
-							<label className={styles.switch} onChange={(e) => handleModeSwitch(e)}>
-								<input type="checkbox" checked={theme}></input>
-								<span className={styles.slider}></span>
-							</label>
-						</li>
-					</ul>
-				</nav>
-			</header>
-		</div>
-	);
+        <nav className={`${styles.cabeceraNav} ${menu ? styles.isActive : ""}`}>
+          <ul className={styles.cabeceraUl}>
+            <li
+              className={`${styles.cabeceraLi} ${
+                currentRoute === "/" ? styles.cabeceraLiActive : ""
+              }`}
+            >
+              <Link className={styles.cabeceraA} href="/">
+                {t("navbar.home")}
+              </Link>
+            </li>
+            <li
+              className={`${styles.cabeceraLi} ${
+                currentRoute === "/about" ? styles.cabeceraLiActive : ""
+              }`}
+            >
+              <Link className={styles.cabeceraA} href="/about">
+                {t("navbar.about")}
+              </Link>
+            </li>
+            <li
+              className={`${styles.cabeceraLi} ${
+                currentRoute === "/contact" ? styles.cabeceraLiActive : ""
+              }`}
+            >
+              <Link className={styles.cabeceraA} href="/contact">
+                {t("navbar.contact")}
+              </Link>
+            </li>
+            <li>
+              <label
+                className={styles.switch}
+                onChange={(e) => handleModeSwitch(e)}
+              >
+                <input type="checkbox" checked={theme}></input>
+                <span className={styles.slider}></span>
+              </label>
+            </li>
+            <li>
+							<LanguageDropdown />
+              {/* <select
+                className={`${styles.select} toTheRight `}
+                onChange={(e) => i18next.changeLanguage(e.target.value)}
+              >
+                <option value="es">ES</option>
+                <option value="en">EN</option>
+                <option value="pt">PT</option>
+              </select> */}
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
 }
